08d31bb3a4b3c024383235339e7ef1458a17b7fe
Hajra <hajraarshad002@gmail.com>
2026-07-24 11:16:49 +0500

    Complete backend project setup

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const pinoHttp = require("pino-http");
 
 const app = express();
 
-// Middleware
+app.use(cors());
 app.use(express.json());
+app.use(pinoHttp());
 
-app.get("/", (req, res) => {
-    res.send("Notes API is running...");
+
+app.get("/", (req,res)=>{
+    res.send("Notes API is running");
 });
+
+
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Server is healthy"
+    });
+});
+
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const app = require("./app");
+const logger = require("./src/logger/logger");
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(PORT,()=>{
+    logger.info(`Server running on port ${PORT}`);
 });
